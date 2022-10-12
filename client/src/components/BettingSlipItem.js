c087cda248cd564a8bb9e95747dd3fcbe8f30630
deni6052 <deni.cosic@gmail.com>
2022-10-12 23:36:32 +0200

    My betting slips

---
+++ client/src/components/BettingSlipItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./BettingSlipItem.css";
+export default function BettingSlipItem({
+  bettingSlip: { betAmount, totalOdds, potentialWinnings, status },
+}) {
+  return (
+    <p className="card betting-slip-item">
+      <span>
+        <b>Bet amount:</b>
+        {betAmount.toFixed(2)}
+      </span>
+      <span>
+        {" "}
+        <b>Total odds:</b>
+        {totalOdds.toFixed(2)}
+      </span>
+      <span>
+        {" "}
+        <b>Potential winnings:</b>
+        {potentialWinnings.toFixed(2)}
+      </span>
+      <span>
+        {" "}
+        <b>Status:</b>
+        {status.replace("_", " ")}
+      </span>
+    </p>
+  );
+}
