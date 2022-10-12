dec58705c064e06f37ee5e7954ee7d92ace220f8
deni6052 <deni.cosic@gmail.com>
2022-10-12 23:37:09 +0200

    Sport events management

--- client/src/views/SportEvents.js
+++ client/src/views/SportEvents.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import "./SportEvents.css";
 
 export default function SportEvents() {
-	return (
-		<div>SportEvents</div>
-	)
+  return (
+    <div className="page">
+      <div className="card"></div>
+      <div className="card sport-events-additional">
+        <h3>Additional actions</h3>
+        <Button label="Resolve events" color="primary" />
+      </div>
+    </div>
+  );
 }
